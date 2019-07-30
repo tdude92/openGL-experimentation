3ec4c0281b9bec79d36b1828f9926c4784d84551
tdude92 <trevorguanhao@gmail.com>
2019-07-30 16:30:30 -0400

    push

---
+++ src/transformations.cpp
@@ -0,0 +1,39 @@
+#define GLEW_STATIC
+
+#include <SFML/Window.hpp>
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "stb_image.h"
+
+#include "shaders.hpp"
+
+int main() {
+    sf::ContextSettings settings;
+    settings.depthBits = 24;
+    settings.stencilBits = 8;
+    settings.antialiasingLevel = 2;
+    settings.majorVersion = 3;
+    settings.minorVersion = 3;
+    settings.attributeFlags = sf::ContextSettings::Core;
+
+    sf::Window root(sf::VideoMode(720, 720), "Transformations", sf::Style::Close, settings);
+
+    glewExperimental = GL_TRUE;
+
+    sf::Event event;
+    bool running = true;
+    while (running) {
+        while (root.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                running = false;
+            }
+        }
+        root.display();
+    }
+
+    return 0;
+}
